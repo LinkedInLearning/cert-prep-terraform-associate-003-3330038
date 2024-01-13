cc13741f4c3a98b3b962532731851784892fd4a2
Helen Chukwukelu <chukwuhely@gmail.com>
2024-01-13 19:42:07 +0100

    added vpc

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "my-custom-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a"]
+  azs             = ["us-east-1a"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
