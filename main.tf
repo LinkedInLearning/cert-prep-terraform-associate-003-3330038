dbd970d9cf6eaf5c626657454b168dc1bd7762b8
Helen Chukwukelu <chukwuhely@gmail.com>
2024-01-19 16:55:45 +0100

    modified backend.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # create vpc
 # terraform aws create vpc
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "my-vpc" {
   cidr_block              = "10.0.0.0/16"
   instance_tenancy        = "default"
   enable_dns_hostnames    = true
 
   tags      = {
-    Name    = "mydemo-vpc"
+    Name    = "mydemo-vpc1"
   }
 }
 
