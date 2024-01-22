89a050f8bdbb06b70cd4895bf894448706cab21e
Helen Chukwukelu <chukwuhely@gmail.com>
2024-01-22 15:12:46 +0100

    added terraform.tfvars

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "bucket_name" {
   description = "Name for the S3 bucket"
   type        = string
-  default     = "terraform-course-demo101"
+  
 }
 
 
@@ -11,8 +11,6 @@ variable "bucket_name" {
 variable "tags" {
   description = "A map of tags for the S3 bucket"
   type        = map(string)
-  default     = {
-    Name        = "terraform-course-demo101"
-    Environment = "Dev"
+  
   }
-}
+
