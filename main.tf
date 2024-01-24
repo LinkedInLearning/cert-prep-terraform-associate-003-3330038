5f032b5ceb895cb795c9125cd7115c328e1af231
Helen Chukwukelu <chukwuhely@gmail.com>
2024-01-24 14:38:45 +0100

    modified config files

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ data "aws_vpc" "default-vpc" {
   default = true
 }
 
-resource "aws_security_group" "PRO-SG" {
-  name        = "PRO-SG"
+resource "aws_security_group" "Project-sg" {
+  name        = "sever1-sg"
   description = "My  security group"
   vpc_id      = data.aws_vpc.default-vpc.id  # Using the VPC ID from the data source
 
