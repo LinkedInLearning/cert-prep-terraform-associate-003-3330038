93a8379c4dccc882354803e8e7d0959f7c3879ab
Helen Chukwukelu <chukwuhely@gmail.com>
2024-03-28 10:31:21 +0100

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,12 @@
+# store the terraform state file in s3 and lock with dynamodb
+terraform {
+  backend "s3" {
+    bucket         = "terraform-demo-s3-bucket11"
+    key            = "terraform-state-dir/terraform.tfstate"
+    region         = "us-east-1"
+    profile        = "default"
+    dynamodb_table = "dynamo-table1"
+  }
+}
+
+
